0cca68626930e1c861d8c91becc1db5f9731353e
Alexandru Botez <botez14alexandru@gmail.com>
2015-09-06 13:23:19 +0300

    added list push, star rating and a custom filter for pagination

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,117 @@
-app.controller('MainController' , function($scope) {
-	
 
-};
+reviewApp.controller('MainController', function($scope) {
+
+	body = angular.element(document.body)
+ 	$scope.stars = [1,2,3,4,5];
+ 	$scope.userReviews = [
+ 	{
+
+ 		userName: 'bap',
+ 		userReview: 'Very nice indeed',
+ 		rating: 3
+
+ 	},
+
+ 	{
+ 		userName: 'illidan',
+ 		userReview: 'Very ddddsacz indeed',
+ 		rating: 4
+ 	},
+ 	{
+
+ 		userName: 'bap',
+ 		userReview: 'Very nice indeed',
+ 		rating: 3
+
+ 	},
+
+ 	{
+ 		userName: 'illidan',
+ 		userReview: 'Very ddddsacz indeed',
+ 		rating: 4
+ 	},
+
+ 	{
+
+ 		userName: 'bap',
+ 		userReview: 'Very nice indeed',
+ 		rating: 3
+
+ 	},
+
+ 	{
+ 		userName: 'illidan',
+ 		userReview: 'Very ddddsacz indeed',
+ 		rating: 4
+ 	},
+
+ 	{
+
+ 		userName: 'bap',
+ 		userReview: 'Very nice indeed',
+ 		rating: 3
+
+ 	},
+
+ 	{
+ 		userName: 'illidan',
+ 		userReview: 'Very ddddsacz indeed',
+ 		rating: 4
+ 	},
+
+ 	{
+
+ 		userName: 'bap',
+ 		userReview: 'Very nice indeed',
+ 		rating: 3
+
+ 	},
+
+ 	{
+ 		userName: 'illidan',
+ 		userReview: 'Very ddddsacz indeed',
+ 		rating: 4
+ 	},
+
+ 	];
+
+ 	 $scope.currentPage = 0;
+ 	 $scope.pageSize = 20;
+ 	 $scope.numberOfPages=function() {
+        return Math.ceil($scope.userReviews.length/$scope.pageSize);                
+    }
+
+
+
+
+
+
+ 	$scope.makeStar = function(nr){
+ 		console.log(nr)
+ 	}
+ 	$scope.updateStar = function(nr){
+ 		$scope.nrStars = nr;
+ 	}
+ 	$scope.addReview = function() {
+ 		$scope.userReviews.push({
+ 			userName: $scope.inputUserName,
+ 			userReview: $scope.inputUserReview,
+ 			rating: $scope.nrStars
+ 		});
+ 		$scope.reviewForm.$setPristine(true);
+ 		$scope.inputUserName = '';
+ 		$scope.inputUserEmail = '';
+ 		$scope.inputUserReview = '';
+ 	}
+ 	
+ 	
+});
+
+
+
+reviewApp.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    }
+});
