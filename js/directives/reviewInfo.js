ca14f4c9296c0f2b1ada41ebf01b6f9591af2306
Alexandru Botez <botez14alexandru@gmail.com>
2015-09-06 13:24:31 +0300

    added checked star loop

--- js/directives/reviewInfo.js
+++ js/directives/reviewInfo.js
@@ -2,8 +2,21 @@ reviewApp.directive('reviewInfo', function() {
   return {
     restrict: 'E',
     scope: {
-      info: '='
+      info: '=',
+      index: '@'
     },
-    templateUrl: 'views/reviewView.html'
+    templateUrl: 'views/reviewView.html',
+    link: function(scope,el,attr,cntr) {
+    
+       scope.makeStar = function(nr){
+       		inputs = el.find('input');
+       		angular.forEach(inputs, function(input, key){
+       			if(key<nr)
+       				input.setAttribute('checked',true)
+
+       		});
+
+       }
+     }
   };
 });
