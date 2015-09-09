44b2e73df8fa0b82fb188ab567ad662a8d55ab8c
Alexandru Botez <botez14alexandru@gmail.com>
2015-09-09 15:30:21 +0300

    Added file after confilts

---
+++ Database/fetchData2.php
@@ -0,0 +1,26 @@
+<?php 
+
+header("Content-Type: application/json; charset=UTF-8");
+include 'connect.php';
+
+$result = mysql_query("SELECT user_name, user_email, user_text_review, star_rating FROM reviews");
+
+
+$rows = array();
+
+while ($row = mysql_fetch_array($result)) {
+	$review = array(
+		"userName" => $row['user_name'],
+		"userEmail" => md5(strtolower(trim($row['user_email']))),
+		"userReview" => $row['user_text_review'],
+		"rating" => $row['star_rating'],
+		);
+	array_push($rows, $review);
+}
+
+echo json_encode($rows);
+
+?>
+
+
+
