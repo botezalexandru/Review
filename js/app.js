9cb1e39af163a7f9f1fc256f68d4f221bff19019
Alexandru Botez <botez14alexandru@gmail.com>
2015-09-15 14:51:24 +0300

    added angular router

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-var reviewApp = angular.module('reviewApp', []);
+var reviewApp = angular.module('reviewApp', ['ngRoute']);
+
+reviewApp.config(function($routeProvider) { 
+  $routeProvider 
+    .when('/', { 
+      controller: 'MainController', 
+      templateUrl: 'views/home.html',  
+      
+    }) 
+
+    .when('/details/:id', {
+      controller: "detailController",
+      templateUrl: "views/detail.html", 
+      
+    })
+
+    .otherwise({ 
+      redirectTo: '/' 
+    }); 
+});
