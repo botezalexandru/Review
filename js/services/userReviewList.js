bc195c9204c26d814aa76405fcf4d4f0e5bbb1e6
Alexandru Botez <botez14alexandru@gmail.com>
2015-09-17 14:57:53 +0300

    service now hold the elements in it

--- js/services/userReviewList.js
+++ js/services/userReviewList.js
@@ -1,8 +1,22 @@
 reviewApp.factory('userReviewList', function($http) {
+	var userReviews = []
+	var databaseSize = 0
 	return {
+		fetch: function(pageSize, begin) {
+			return $http.get("Database/fetchData.php", {
+				params: { limit: 		pageSize,
+						  beginWithRow: begin }
+			}).success(function(response) {
+				userReviews = response.review;
+				userReviews.reverse();
+				databaseSize = parseInt(response.rowCount);
+			});
+		},
 		get: function() {
-			console.log("inside function");
-			return $http.get("Database/fetchData.php");
+			return userReviews
+		},
+		getDbSize: function() {
+			return databaseSize
 		}
 	}
 	
